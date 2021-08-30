f5e78895f77df7d45db8be84103816eaaccd60a5
Alexandra Volentir <67002616+AlexandraVolentir@users.noreply.github.com>
2021-08-30 11:45:51 +0300

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,7 +1,7 @@
-
 public class Flight {
-
-    // something about the date
-    //
-
+    private DateAndTime start_date;
+    private DateAndTime arrival_date;
+    private AvailableSeat seats;
+    private Ticket[] crew;
+    private Plane plane;
 }
