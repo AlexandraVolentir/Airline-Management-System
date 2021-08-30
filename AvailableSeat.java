a6f386c55ada5469c32a6b7e1ddd0b051fd00409
Alexandra Volentir <67002616+AlexandraVolentir@users.noreply.github.com>
2021-08-30 11:44:44 +0300

    Add files via upload

---
+++ AvailableSeat.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+
+public class AvailableSeat {
+    private HashMap<Seat, Boolean> seats = new HashMap<Seat, Boolean>();
+    public void book(Seat seat){}
+    public void unbook(Seat seat){}
+}
