0767f08c4bed7cfef8011ed7b66aa85ae130df67
Alexandra Volentir <67002616+AlexandraVolentir@users.noreply.github.com>
2021-08-30 11:49:08 +0300

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,12 +1,47 @@
 public class Ticket {
-    private Flight flight_information;
-    private Seat seat_information;
-    private Airline company;
-    private Passenger client;
+    private Passenger who;
+    private DateAndTime departure;
+    private DateAndTime landing;
+    private Seat confirmed_seat;
+    public boolean isValid;
 
-    public Ticket() {
-        //
+    public Passenger getWho() {
+        return who;
     }
 
+    public void setWho(Passenger who) {
+        this.who = who;
+    }
 
+    public DateAndTime getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(DateAndTime departure) {
+        this.departure = departure;
+    }
+
+    public DateAndTime getLanding() {
+        return landing;
+    }
+
+    public void setLanding(DateAndTime landing) {
+        this.landing = landing;
+    }
+
+    public Seat getConfirmed_seat() {
+        return confirmed_seat;
+    }
+
+    public void setConfirmed_seat(Seat confirmed_seat) {
+        this.confirmed_seat = confirmed_seat;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
 }
