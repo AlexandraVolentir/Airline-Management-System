52501736fb241d321fec156f343654ea62b102cb
Alexandra Volentir <67002616+AlexandraVolentir@users.noreply.github.com>
2021-09-10 15:49:49 +0300

    Delete AvailableSeat.java

--- AvailableSeat.java
+++
@@ -1,7 +0,0 @@
-import java.util.HashMap;
-
-public class AvailableSeat {
-    private HashMap<Seat, Boolean> seats = new HashMap<Seat, Boolean>();
-    public void book(Seat seat){}
-    public void unbook(Seat seat){}
-}
