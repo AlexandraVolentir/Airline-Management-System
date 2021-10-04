d2de25df9a28dd05799dab08d418d2a72e3b7e38
Alexandra Volentir <67002616+AlexandraVolentir@users.noreply.github.com>
2021-10-04 11:58:43 +0300

    Some methods related to seat distribution

--- src/main/Plane.java
+++ src/main/Plane.java
@@ -2,32 +2,64 @@ package src.main;
 
 public class Plane {
 
-    private StringBuilder name;
+    private StringBuilder planeName;
     private StringBuilder company;
+    int rows;
+    int columns;
+    private final Seat[][] seats = new Seat[rows][columns]; // 9 for ABC DEF GHI
+    Seat lastAvailSeat;
+    int totalNumberOfSeats;
+    int bookedSeats = 0;
+    
 
-    public Plane(StringBuilder name, StringBuilder company) {
-        setName(name);
-        setCompany(company);
+    public Plane(StringBuilder name, StringBuilder company, int rows, int columns) {
+        setPlaneName(name); setCompany(company);
+        setRows(rows); setColumns(columns);
+        initializeSeats();
     }
 
-    public void checkTheNameForValidity(StringBuilder name) throws Exception {
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public int getTotalNumberOfSeats() {
+        return totalNumberOfSeats;
+    }
+
+    public void setTotalNumberOfSeats() {
+        this.totalNumberOfSeats = getColumns()*getRows();
+    }
+
+    public void checkPlaneNameForValidity(StringBuilder name) throws Exception {
         if (name.length() > 20) {
             throw new Exception("Your input is invalid. Try again.");
         }
     }
 
-    public StringBuilder getName() {
-        return name;
+    public StringBuilder getPlaneName() {
+        return planeName;
     }
 
-    public void setName(StringBuilder name) {
+    public void setPlaneName(StringBuilder name) {
 
         try {
-            checkTheNameForValidity(name);
+            checkPlaneNameForValidity(name);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        this.name = name;
+        this.planeName = name;
     }
 
     public StringBuilder getCompany() {
@@ -36,10 +68,45 @@ public class Plane {
 
     public void setCompany(StringBuilder company) {
         try {
-            checkTheNameForValidity(company);
+            checkPlaneNameForValidity(company);
         } catch (Exception e) {
             e.printStackTrace();
         }
         this.company = company;
     }
+
+    public void initializeSeats(){
+
+        setTotalNumberOfSeats();
+        for(int i = 0; i< rows; i++)
+            for(int j = 0; j< columns; j++){
+                seats[i][j] = new Seat(i, attributeColumn(j), false);
+            }
+        lastAvailSeat = seats[0]['A'];
+    }
+    
+    public char attributeColumn(int column) {
+        if (column < columns)
+            return (char) (column + 'A');
+        return '-';
+    }
+
+    public boolean bookSeat(int row, int column) {
+        if(bookedSeats == totalNumberOfSeats){
+            return false;
+        }
+        else{
+            ////////////////////////////////////
+            if (checkSeatForAvailability(++lastAvailSeat.row, ++lastAvailSeat.column))
+                seats[row][column] = new Seat(row, attributeColumn(column), true);
+            bookedSeats++;
+            return true;
+        }
+       
+    }
+
+    public boolean checkSeatForAvailability(int cur_row, int cur_column) {
+        return seats[cur_row][cur_column].row != 0 || seats[cur_row][cur_column].column != '\u0000';
+    }
 }
+
