b5ee742af2a17a2a085cd22d88fdcceb27c10b41
radu781 <poparadu501@gmail.com>
2021-10-07 23:02:50 +0300

    Updated .gitignore and added new tests

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,25 +3,51 @@ package src.test;
 import src.main.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        CSVReader r = new CSVReader("src\\resources\\csv\\countries.csv");
-        Passenger p = new Passenger(r);
+    public static void main(String[] args) {
+        Test t = new Test();
+        try {
+            t.testPassenger();
+            t.testCSV();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        p.setFirstName("Robert");
-        p.setLastName("Robertson");
-        p.setAddress(new Address(r));
+    static class Test {
+        private void testPassenger() throws Exception {
+            CSVReader r = new CSVReader("countries.csv");
+            Passenger p = new Passenger(r);
 
-        p.setDestination(new StringBuilder("United Kingdom"), new StringBuilder("York"));
-        p.setSSN(12309832);
-        p.setSex('m');
+            p.setFirstName("Robert");
+            p.setLastName("Robertson");
+            p.setAddress(new Address(r));
 
-        System.out.println(p.getDestination().getCity() + ", " + p.getDestination().getCountry());
-        System.out.println(p.toString());
+            p.setDestination(new StringBuilder("United Kingdom"), new StringBuilder("York"));
+            p.setSSN(12309832);
+            p.setSex('m');
 
-        Ticket t = new Ticket();
-        t.setWho(p);
+            System.out.println(p.getDestination().getCity() + ", " + p.getDestination().getCountry());
+            System.out.println(p.toString());
 
-        Flight f = new Flight(new DateAndTime(2021, 12, 3, 7, 4), new DateAndTime(2021, 12, 3, 10, 30), new Plane(new StringBuilder("Plane name"), new StringBuilder("Plane company")));
-        f.addTicket(t);
+            Ticket t = new Ticket();
+            t.setWho(p);
+
+            Flight f = new Flight(new DateAndTime(2021, 12, 3, 7, 4), new DateAndTime(2021, 12, 3, 10, 30),
+                    new Plane(new StringBuilder("Plane name"), new StringBuilder("Plane company")));
+            f.addTicket(t);
+        }
+
+        private void testCSV() throws Exception {
+            CSVReader r = new CSVReader("test.csv");
+            r.clear();
+            String test = "Test, append";
+            r.append(test);
+            r.append(new String[] { "country,city,digits", "united kingdom,london,123", "romania,bucharest,456" });
+            System.out.println(r.findFirstLine("country,city,digits"));
+            for (String string : r.getLine(2)) {
+                System.out.println(string);
+            }
+            r.removeAt(2);
+        }
     }
 }
