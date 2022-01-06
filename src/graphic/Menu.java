f947b52f696b163c985945bb1bf674a7ef2987a7
Alexandra Volentir <67002616+AlexandraVolentir@users.noreply.github.com>
2022-01-06 23:15:19 +0200

    idle

--- src/graphic/Menu.java
+++ src/graphic/Menu.java
@@ -1,18 +1,20 @@
-package graphic;
-import javax.swing.*;
-import main.*;
-
-public class Menu {
-    public static void main(String[] args) {
-        JFrame f=new JFrame();//creating instance of JFrame
-
-        JButton b=new JButton("Reserve ticket");//creating instance of JButton
-        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
-
-        f.add(b);//adding button in JFrame
-
-        f.setSize(400,500);//400 width and 500 height
-        f.setLayout(null);//using no layout managers
-        f.setVisible(true);//making the frame visible
-    }
-}
+import javax.swing.*;  
+public class Simple {  
+JFrame f;  
+Simple(){  
+f=new JFrame();//creating instance of JFrame  
+          
+JButton b=new JButton("click");//creating instance of JButton  
+b.setBounds(130,100,100, 40);  
+          
+f.add(b);//adding button in JFrame  
+          
+f.setSize(400,500);//400 width and 500 height  
+f.setLayout(null);//using no layout managers  
+f.setVisible(true);//making the frame visible  
+}  
+  
+public static void main(String[] args) {  
+new Simple();  
+}  
+}  
